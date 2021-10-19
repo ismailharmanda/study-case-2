989632519636979a5a7b4e20a5c5f26e60bfc933
ismail harmanda <iharmanda@hotmail.com>
2021-10-19 16:13:10 +0300

    create dummy products list

---
+++ src/utils/_products.js
@@ -0,0 +1,103 @@
+import img1 from "./assets/products/hoodie-red.png";
+import img2 from "./assets/products/sweatshirt-black-zip.png";
+import img3 from "./assets/products/sweatshirt-gray-zip.png";
+import img4 from "./assets/products/sweatshirt-khaki.png";
+import img5 from "./assets/products/hoodie-black.png";
+import img6 from "./assets/products/hoodie-blue.png";
+import img7 from "./assets/products/sweatshirt-black.png";
+import img8 from "./assets/products/sweatshirt-black-zip-2.png";
+import img9 from "./assets/products/sweatshirt-black-ataturk.png";
+import img10 from "./assets/products/sweatshirt-white-ataturk.png";
+
+export const _products = [
+  {
+    id: 1,
+    name: "Kapüşonlu Uzun Kollu Baskılı Kalın Erkek Sweatshirt",
+    price: 119.99,
+    img: img1,
+    colors: ["red"],
+    isFavorite: false,
+    onCart: false,
+  },
+  {
+    id: 2,
+    name: "LCW BASIC Kapüşonlu Uzun Kollu Fermuarlı Erkek Sweatshirt",
+    price: 109.99,
+    img: img2,
+    colors: ["black", "white"],
+    isFavorite: false,
+    onCart: false,
+  },
+  {
+    id: 3,
+    name: "LCW BASIC Kapüşonlu Uzun Kollu Fermuarlı Erkek Sweatshirt",
+    price: 119.99,
+    img: img3,
+    colors: ["gray", "white"],
+    isFavorite: false,
+    onCart: false,
+  },
+  {
+    id: 4,
+    name: "LCW BASIC Bisiklet Yaka Uzun Kollu Kalın Erkek Sweatshirt",
+    price: 89.99,
+    img: img4,
+    colors: ["khaki"],
+    isFavorite: false,
+    onCart: false,
+  },
+  {
+    id: 5,
+    name: "LCW CASUAL Kapüşonlu Uzun Kollu Baskılı Erkek Sweatshirt",
+    price: 119.99,
+    img: img5,
+    colors: ["black", "white"],
+    isFavorite: false,
+    onCart: false,
+  },
+  {
+    id: 6,
+    name: "LCW CASUAL Kapüşonlu Uzun Kollu Baskılı Erkek Sweatshirt",
+    price: 119.99,
+    img: img6,
+    colors: ["blue", "yellow"],
+    isFavorite: false,
+    onCart: false,
+  },
+  {
+    id: 7,
+    name: "LCW CASUAL Bisiklet Yaka Uzun Kollu Erkek Sweatshirt",
+    price: 69.99,
+    img: img7,
+    colors: ["black"],
+    isFavorite: false,
+    onCart: false,
+  },
+  {
+    id: 8,
+    name: "LCW CASUAL Kapüşonlu Fermuarlı Erkek Sweatshirt",
+    price: 109.99,
+    img: img8,
+    colors: ["black"],
+    isFavorite: false,
+    onCart: false,
+  },
+  {
+    id: 9,
+    name: "Atatürk Baskılı Kapüşonlu Sweatshirt",
+    price: 99.99,
+    img: img9,
+    colors: ["black", "white"],
+    isFavorite: false,
+    onCart: false,
+  },
+  {
+    id: 10,
+    name: "Kapüşonlu Uzun Kollu Baskılı Kalın Erkek Sweatshirt",
+    price: 119.99,
+    img: img10,
+    colors: ["white", "black"],
+    isFavorite: false,
+    onCart: false,
+  },
+];
